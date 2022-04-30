bcc1fa11db38f5521865d85d2278d42002808bc3
Ettore Galli <ettore.galli@gmail.com>
2022-04-30 17:44:05 +0200

    Basic workflow

--- workflow_python/services/order.py
+++ workflow_python/services/order.py
@@ -5,7 +5,7 @@ import requests
 
 
 def get_order(service_url: str, order_number: int):
-    query_url = service_url.replace("#order_number", str(order_number))
+    query_url = service_url.replace("order_number", str(order_number))
     print(query_url)
     response = requests.get(query_url)
     return json.dumps(json.loads(response.text))
@@ -13,5 +13,5 @@ def get_order(service_url: str, order_number: int):
 
 if __name__ == '__main__':
     print(
-        get_order("http://localhost:8000/orders/#order_number", 2)
+        get_order("http://localhost:8000/orders/order_number", 2)
     )
--- workflow_python/workflow.py
+++ workflow_python/workflow.py
@@ -4,74 +4,85 @@ from dataclasses import dataclass
 import os
 from typing import List, Optional, Any, Callable, Dict
 
-from config import read_config
-from logger import log
-from reader import read_input
-from writer import write_output
+from services.config import read_config
+from services.order import get_order
+from services.exchange import get_exchange
+from services.price import get_price
+from services.writer import write_output
 
 
-class IO:
-    def __init__(self, data: Optional[Any] = None):
-        self.__data: Optional[Any] = data
+class WorkflowPayload:
+
+    def __init__(self, config: Optional[Any] = None, data: Optional[Any] = None):
+        self.data: Optional[Any] = data
+        self.config: Optional[Any] = config
+
+    def __repr__(self):
+        return str(self.config) + str(self.data)
 
     @classmethod
-    def unit(cls, data: Any):
-        return IO(data)
+    def unit(cls, config: Any, data: Any):
+        return WorkflowPayload(config, data)
 
-    def bind(self, f: Callable[[Any], IO]):
-        return f(self.__data)
+    def bind(self, f: Callable[[WorkflowPayload], WorkflowPayload]):
+        return f(self)
 
     def __rshift__(self, other):
         return self.bind(other)
 
 
-def read_config_action() -> IO:
-    return IO(data=read_config("./cfg/config.ini"))
+def read_config_action(payload: WorkflowPayload) -> WorkflowPayload:
+    return WorkflowPayload(config=read_config("./cfg/config.ini"))
 
 
-def read_input_action(input_file_name: str) -> IO:
-    return IO(data=list(read_input(input_file_name)))
+def read_order_action(payload: WorkflowPayload) -> WorkflowPayload:
+    return WorkflowPayload(data=get_order(service_url=payload.config["orders_url"], order_number=1))
 
 
-def print_config_action(config: Dict) -> IO:
-    print(config)
-    return IO()
+# def print_config_action(config: Dict) -> WorkflowPayload:
+#     print(config)
+#     return WorkflowPayload()
 
 
-def print_input_action(input: List) -> IO:
-    for item in input:
-        print(item)
-    return IO()
+# def print_input_action(input: List) -> WorkflowPayload:
+#     for item in input:
+#         print(item)
+#     return WorkflowPayload()
 
 
-def compose_output_file_name(entry: str, config: Dict):
-    return os.path.join(
-        config["output_directory"],
-        f'{config["output_file_prefix"]}_{entry[:10].strip()}.txt',
-    )
+# def compose_output_file_name(entry: str, config: Dict):
+#     return os.path.join(
+#         config["output_directory"],
+#         f'{config["output_file_prefix"]}_{entry[:10].strip()}.txt',
+#     )
 
 
-def write_output_action(cfg: Dict, input: List) -> IO:
-    for entry_data in input:
-        output_file_name = compose_output_file_name(entry_data, cfg)
-        print(f"Writing {output_file_name}")
-        write_output(entry_data, output_file_name)
+# def write_output_action(cfg: Dict, input: List) -> WorkflowPayload:
+#     for entry_data in input:
+#         output_file_name = compose_output_file_name(entry_data, cfg)
+#         print(f"Writing {output_file_name}")
+#         write_output(entry_data, output_file_name)
 
-    return IO()
-
-
-def workflow() -> IO:
-    read_config_action().bind(
-        lambda c: read_input_action(c["input_file"]).bind(
-            lambda input: print_config_action(c).bind(
-                lambda _: print_input_action(input).bind(
-                    lambda _: write_output_action(c, input)
-                )
-            )
-        )
-    )
+#     return WorkflowPayload()
 
 
+# def workflow() -> WorkflowPayload:
+#     read_config_action().bind(
+
+
+# def workflow() -> WorkflowPayload:
+#     read_config_action().bind(
+#         lambda c: read_input_action(c["input_file"]).bind(
+#             lambda input: print_config_action(c).bind(
+#                 lambda _: print_input_action(input).bind(
+#                     lambda _: write_output_action(c, input)
+#                 )
+#             )
+#         )
+#     )
 if __name__ == "__main__":
     print(os.path.abspath("."))
-    workflow()
+    print(
+        WorkflowPayload.unit({}, {}).bind(
+            read_config_action).bind(read_order_action)
+    )
