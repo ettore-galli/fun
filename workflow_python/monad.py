c6e11e362db588d22280c69fdd0ce4043638c61b
Ettore Galli <ettore.galli@gmail.com>
2022-06-03 23:13:12 +0200

    __or__

--- workflow_python/monad.py
+++ workflow_python/monad.py
@@ -4,15 +4,23 @@ from typing import Callable, Any
 
 
 class Monad:
-    def __init__(self, value):
+    def __init__(self, value=None, success: bool = True):
         self.value = value
+        self.success = success
 
     @classmethod
     def unit(cls, value: Any = None):
         return Monad(value)
 
+    @staticmethod
+    def _merge_data(current: Monad, new: Monad):
+        return Monad(new.value or current.value, success=current.success and new.success)
+
     def bind(self, f: Callable[[Any], Monad]):
-        return f(self.value)
+        return Monad._merge_data(self, f(self.value))
+
+    def __or__(self, f):
+        return self.bind(f)
 
 
 def pure_input(_):
@@ -25,4 +33,7 @@ def pure_print(text: str):
 
 
 if __name__ == '__main__':
-    Monad.unit("start-value").bind(pure_input).bind(pure_print)
+    print("1 ---")
+    Monad().bind(pure_input).bind(pure_print)
+    print("2 ---")
+    Monad() | pure_input | pure_print
